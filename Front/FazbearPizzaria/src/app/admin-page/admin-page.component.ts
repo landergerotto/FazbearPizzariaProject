e24d753c6ffdb74ffabebcc13d3c328d7ba7ffd8
Lander <lander.gerotto@gmail.com>
2023-11-20 16:19:03 -0300

    Finished Log in Page + Started Admin Page

---
+++ Front/FazbearPizzaria/src/app/admin-page/admin-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-admin-page',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule,
+    FormsModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './admin-page.component.html',
+  styleUrl: './admin-page.component.css'
+})
+export class AdminPageComponent {
+  hide=true;
+}
--- Front/FazbearPizzaria/src/app/app.component.ts
+++ Front/FazbearPizzaria/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginPageComponent, AdminPageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

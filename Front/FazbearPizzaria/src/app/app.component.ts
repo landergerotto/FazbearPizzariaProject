482a83370155922979f0058eb327d14e94848f21
Lander <lander.gerotto@gmail.com>
2023-11-20 16:53:30 -0300

    Last thing of the day commit

--- Front/FazbearPizzaria/src/app/app.component.ts
+++ Front/FazbearPizzaria/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
+import { PedidosPageComponent } from './pedidos-page/pedidos-page.component';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginPageComponent, AdminPageComponent],
+  imports: [CommonModule, RouterOutlet, LoginPageComponent, AdminPageComponent, PedidosPageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

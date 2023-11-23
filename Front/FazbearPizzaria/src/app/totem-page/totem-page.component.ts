1f924d0f7f42de8c07cfbf08ff0b071bd517bd98
Lander <lander.gerotto@gmail.com>
2023-11-23 16:01:09 -0300

    Finished Totem Page

--- Front/FazbearPizzaria/src/app/totem-page/totem-page.component.ts
+++ Front/FazbearPizzaria/src/app/totem-page/totem-page.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+
 @Component({
   selector: 'app-totem-page',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, ScrollingModule],
   templateUrl: './totem-page.component.html',
   styleUrl: './totem-page.component.css'
 })
 export class TotemPageComponent {
-
+  items = Array.from({length: 100}).map((_, i) => `Item #${i}`);
 }

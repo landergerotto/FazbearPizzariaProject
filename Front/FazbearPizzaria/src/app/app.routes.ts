b1b31d407ee26a21734f5402fb4505b548b49226
Lander <lander.gerotto@gmail.com>
2023-11-23 16:50:42 -0300

    Started ProdutosPage

--- Front/FazbearPizzaria/src/app/app.routes.ts
+++ Front/FazbearPizzaria/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import { PedidosPageComponent } from './pedidos-page/pedidos-page.component';
 import { TotemPageComponent } from './totem-page/totem-page.component';
+import { ProdutosPageComponent } from './produtos-page/produtos-page.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
   {path: 'admin', component: AdminPageComponent},
   {path:'pedidos', component: PedidosPageComponent},
   {path:'totem', component: TotemPageComponent},
+  {path:'produtos', component: ProdutosPageComponent},
+
 
 ];
--- Front/FazbearPizzaria/src/app/produtos-page/produtos-page.component.ts
+++ Front/FazbearPizzaria/src/app/produtos-page/produtos-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-produtos-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTabsModule],
   templateUrl: './produtos-page.component.html',
   styleUrl: './produtos-page.component.css'
 })

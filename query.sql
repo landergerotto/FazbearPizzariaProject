18362cf28f1989727874295cb655677bc3bf49ae
Lander <lander.gerotto@gmail.com>
2023-11-24 15:48:09 -0300

    Added query

---
+++ query.sql
@@ -0,0 +1,58 @@
+use master
+go
+
+if exists(select * from sys.databases where name = 'FreddyFazbearDB')
+	drop database FreddyFazbearDB
+go
+
+create database FreddyFazbear
+go
+
+use FreddyFazbearDB
+go
+
+create table Imagem(
+	ID int identity primary key,
+	Foto varbinary(MAX) not null
+);
+go
+
+create table Usuario(
+	ID int identity primary key,
+	Nome varchar(80) not null,
+	Senha varchar(MAX) not null,
+	Salt varchar(200) not null,
+	Adm bit not null,
+	ImagemID int references Imagem(ID)
+);
+go
+
+create table Produto (
+	ID int identity primary key,
+	Nome varchar(50) not null,
+	Tipo varchar(25) not null,
+	Preco decimal (5, 2) not null,
+	Descricao varchar(MAX),
+	ImagemID int references Imagem(ID)
+)
+
+create table Pedido (
+	ID int identity primary key,
+	PrecoTotal float,
+	Preparado bit not null,
+	Entregue bit not null
+)
+create table Promocao(
+	ID int identity primary key,
+	ProdutoID int references Produto(ID),
+	Preco int not null,
+);
+go
+
+create table ProdutosPedidos (
+	ID int identity primary key,
+	ProdutoID int references Produto(ID),
+	PedidoID int references Pedido(ID),
+	PromocaoID int references Promocao(ID)
+)
+

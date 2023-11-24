5cd341f5b276a698a90c6169f677e43dca9edfa8
Lander <lander.gerotto@gmail.com>
2023-11-24 16:53:04 -0300

    Last commit of the day

--- Back/Controllers/UserController.cs
+++ Back/Controllers/UserController.cs
@@ -1,1 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+
+using DTO;
+
+namespace Back.Controllers;
+[ApiController]
+[Route("user")]
+public class UserController : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login([FromBody]UserData user)
+    {
+        return Ok();
+    }
+}
---
+++ DTO/UserData.cs
@@ -0,0 +1,7 @@
+namespace DTO;
+
+public class UserData
+{
+    public string Name { get; set; }
+    public string Passwrod { get; set; }
+}
